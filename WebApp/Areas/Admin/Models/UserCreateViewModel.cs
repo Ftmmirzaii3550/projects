0e5d203712de57f8116a828f1232036c9772b651
Ftmmirzaii3550 <148806245+Ftmmirzaii3550@users.noreply.github.com>
2024-01-31 14:52:03 +0330

    git commit all project

---
+++ WebApp/Areas/Admin/Models/UserCreateViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Areas.Admin.Models
+{
+    public class UserCreateViewModel
+    {
+
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+
+        [Display(Name = "نام")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "تکمیل فیلد {0} الزامی است")]
+        public string FirstName { get; set; }
+
+
+        [Display(Name = "نام خانوادگی")]
+        public string LastName { get; set; }
+
+        [Display(Name = "رمز عبور")]
+        [DataType(DataType.Password)]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "تکمیل فیلد {0} الزامی است")]
+        public string Password { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "تایید رمز عبور")]
+        [Compare(nameof(Password), ErrorMessage = "{0} تطابق ندارد")]
+        public string ConfirmPassword { get; set; }
+    }
+}
